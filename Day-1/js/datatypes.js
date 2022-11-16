08f70a48e7093f58f1a4b343fc6f64ef190b0cfb
Aziz Abduqahhorov <83882515+abduqahhorovaziz@users.noreply.github.com>
2022-11-16 15:32:31 +0500

    day 1 done

---
+++ Day-1/js/datatypes.js
@@ -0,0 +1,8 @@
+/* ================= Task 5 ================= */
+
+// console.log(typeof(firstName))
+// console.log(typeof(lastName))
+// console.log(typeof(age))
+// console.log(typeof(PI))
+// console.log(typeof(emptyValue))
+// console.log(typeof(undVariable))
---
+++ Day-1/js/variable.js
@@ -0,0 +1,7 @@
+/* ================= Task 4 ================= */
+// let firstName = "Tony";
+// let lastName = "Stark";
+// let age = 30;
+// const PI = 3.14;
+// let emptyValue = null;
+// let undVariable;
